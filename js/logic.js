08688fe679ae5f46fc52c235a862053a25ea0d7f
Robert Petermeier <robert.petermeier@gmx.de>
2013-01-03 10:26:27 +0100

    Map data is loaded from a JSON file

--- js/logic.js
+++ js/logic.js
@@ -3,6 +3,28 @@ function init() {
 }
 
 function draw() {
+	d3.json("json/map.json", function(json) {
+		drawMap(json);
+	});
+}
+
+function drawMap(mapDataJson) {
+	var svgContainer = d3.select("#map");
+	var borders = svgContainer.selectAll("line")
+							.data(mapDataJson.lines)
+							.enter()
+							.append("line");
+	borders.attr("x1", function(d) { return d.x1 })
+		.attr("y1", function(d) { return d.y1; })
+		.attr("x2", function(d) { return d.x2; })
+		.attr("y2", function(d) { return d.y2; })
+		.attr("stroke-width", 2)
+		.attr("stroke", "black");
+
+	alert(borders);
+}
+
+function drawStatic() {
 	// var svgContainer = d3.select("body").append("svg")
     //                                .attr("width", 900)
     //                               .attr("height", 800);
@@ -15,5 +37,4 @@ function draw() {
                         .attr("y2", 10)
 						.attr("stroke-width", 2)
                         .attr("stroke", "black");
-	alert(line);
 }
